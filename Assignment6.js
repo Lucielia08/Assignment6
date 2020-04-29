ae6f3f4f665e028484d9ea4fe74e38d29aedd405
lucielia08 <llucielia08@gmail.com>
2020-04-28 21:34:14 -0400

    Created math functions and console.log results

--- Assignment6.js
+++ Assignment6.js
@@ -3,24 +3,24 @@
 // each function should take in 2 input, an 'x' and an 'y'
 // at the end of each function, output the result using console log
 
-function add(/* Fill in the input*/){
-
+function add(x,y){
+console.log(x+y);
 }
 
-function minus(/* Fill in the input*/){
-
+function minus(x,y){
+console.log(x-y);
 }
 
-function multiply(/* Fill in the input*/){
-
+function multiply(x,y){
+console.log(x*y);
 }
 
-function divide(/*Fill in the input*/){
-
+function divide(x,y){
+console.log(x/y);
 }
 
 // Test cases:
-// add(1,9); ---> 10
-// minus(5, 3); ---> 2
-// multiply(2,4); ---> 8
-// divide(10, 2); ---> 5
+add(1,9);
+minus(5,3);
+multiply(2,4);
+divide(10,2);
